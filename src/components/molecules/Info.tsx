a0355d33223f0b958f6b07bac0d56e20031540ef
= <=>
2024-12-05 16:41:39 +0000

    deplyment check

--- src/components/molecules/Info.tsx
+++ src/components/molecules/Info.tsx
@@ -1,40 +1,37 @@
 import { InputField } from "../atoms/InputField/InputField";
 import "../../App.css";
-import { usePersistedState } from "../functions/Data";
 
 function Info() {
-  const [name, setname] = usePersistedState("name", "");
-  const [address, setaddres] = usePersistedState("address", "");
-  const [number, setnumber] = usePersistedState("number", "");
+ 
   return (
     <div className="slide-in">
       <InputField
         name="Name"
-        onChange={event => {
-          setname(event.target.value);
+        onChange={() => {
+        
         }}
         placeholder="essel apusiga"
         type="text"
-        value={name}
+        value={''}
       />
 
       <InputField
         name="Email Address"
-        onChange={event => {
-          setaddres(event.target.value);
+        onChange={() => {
+         
         }}
         placeholder="essel.abraham@amalitech.com"
         type="email"
-        value={address}
+        value={''}
       />
       <InputField
         name="Phone Number"
-        onChange={event => {
-          setnumber(event.target.value);
+        onChange={() => {
+         
         }}
         placeholder="+233532911103"
         type="text"
-        value={number}
+        value={''}
       />
     </div>
   );
