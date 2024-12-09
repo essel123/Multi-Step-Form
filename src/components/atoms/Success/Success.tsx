388ae437b5359704ef8d1c6dce4653d548fcb2aa
= <=>
2024-12-09 12:10:08 +0000

    responsive check

--- src/components/atoms/Success/Success.tsx
+++ src/components/atoms/Success/Success.tsx
@@ -5,7 +5,7 @@ export default function Success() {
   return (
     <div className="slide-in">
       <section className={styles.section}>
-        <img src="../../../../public/images/icon-thank-you.svg" alt="" />
+        <img src="./images/icon-thank-you.svg" alt="" />
         <h1 className={styles.thanks}>Thank you!</h1>
         <p className={styles.thanksMessage}>
           Thanks for confirming your subscription! We hope you have fun using
