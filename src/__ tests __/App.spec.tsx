a5af888c7c156eab123a8811924d855bf1c861f5
= <=>
2024-12-17 14:59:57 +0000

    commemnted code removeed

--- src/__ tests __/App.spec.tsx
+++ src/__ tests __/App.spec.tsx
@@ -2,7 +2,7 @@ import {
   Steps,
   StepDetails,
   PlanOption,
-  Fields,
+ 
   addonData
 } from "../function/Functions"; // Adjust import path as necessary
 
@@ -84,48 +84,8 @@ describe("PlanOption function", () => {
     expect(result).toEqual(expected);
   });
 });
-beforeEach(() => {
-  Storage.prototype.getItem = jest.fn().mockImplementation((key: number) => {
-    const mockData = {
-      name: "essel apusiga",
-      address: "essel.abraham@amalitech.com",
-      number: "+233532911103"
-    };
-    return mockData[key];
-  });
 
-  Storage.prototype.setItem = jest.fn(); // Mock the setItem function
-});
 
-describe("Fields function", () => {
-  it("should return the correct JSON structure", () => {
-    const expected = [
-      {
-        name: "Name",
-        type: "text",
-        placeholder: "essel apusiga",
-        value: "essel apusiga"
-      },
-      {
-        name: "Email Address",
-        type: "email",
-        placeholder: "essel.abraham@amalitech.com",
-        value: "essel.abraham@amalitech.com"
-      },
-      {
-        name: "Phone Number",
-        type: "text",
-        placeholder: "+233532911103",
-        value: "+233532911103"
-      }
-    ];
-
-    const result = Fields();
-
-    // Check if the result matches the expected data
-    expect(result).toEqual(expected);
-  });
-});
 describe("addonData function", () => {
   it("should return the correct JSON structure", () => {
     const expected = [
